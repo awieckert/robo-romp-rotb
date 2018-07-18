78bfa1cedf26e2c832a9013b15f183a5df3d458b
Adam Wieckert <wiec1369@gmail.com>
2018-07-17 19:32:17 -0500

    finished the components and the routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Redirect, Switch}  from 'react-router-dom';
+import Home from '../components/Home/Home.js';
+import Login from '../components/Login/Login.js';
+import Register from '../components/Register/Register.js';
+import GameMode from '../components/GameMode/GameMode.js';
+import SelectionScreen from '../components/SelectionScreen/SelectionScreen.js';
+import FightArena from '../components/FightArena/FightArena.js';
+import WinnerScreen from '../components/WinnerScreen/WinnerScreen.js';
 import './App.css';
 
+const PublicRoute = ({component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{pathname: '/gamemode', state: {from: props.location}}} />
+        )
+      }
+    />
+  );
+};
+
+const PrivateRoute = ({component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{pathname: '/', state: {from: props.location}}} />
+        )
+      }
+    />
+  );
+};
+
 class App extends Component {
+  state = {
+    authed: false,
+    userProfile: {},
+    userRobot: {},
+    enemyProfile: {},
+    enemyRobot: {},
+  }
+
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <div className='container-fluid main-container'>
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <PublicRoute path='/login' authed={this.state.authed} component={Login} />
+              <PublicRoute path='/register' authed={this.state.authed} component={Register} />
+              <PrivateRoute path='/gamemode' authed={this.state.authed} component={GameMode} />
+              <PrivateRoute path='/selectionscreen' authed={this.state.authed} component={SelectionScreen}/>
+              <PrivateRoute path='/fightarena' authed={this.state.authed} component={FightArena} />
+              <PrivateRoute path='/winnerscreen' authed={this.state.authed} component={WinnerScreen}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
