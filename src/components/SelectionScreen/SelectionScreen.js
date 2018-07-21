09bef39c695433e3e1e65ee00425fd20bc5bacf5
Adam Wieckert <wiec1369@gmail.com>
2018-07-21 15:24:52 -0500

    all robot data is loaded into state. printing small robot icons

--- src/components/SelectionScreen/SelectionScreen.js
+++ src/components/SelectionScreen/SelectionScreen.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import robotRequests from '../../firebaseRequests/robotRequests.js';
+import SmallBot from '../SmallBot/SmallBot.js';
 import './SelectionScreen.css';
 
 class SelectionScreen extends Component {
+  state = {
+    allRobots: [],
+  };
+
+  componentDidMount () {
+    robotRequests.getRobots().then((robots) => {
+      this.setState({allRobots: robots});
+    }).catch((err) => {
+      console.error('Could not get robots from firebase: ', err);
+    });
+  };
+
   render () {
     return (
       <div className="SelectionScreen">
         <h1 className="SelectionScreen-title">SelectionScreen</h1>
+        <div className='row navbar-fixed-bottom'>
+          <div className='col-xs-12 row'>
+            <SmallBot bots={this.state.allRobots} />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/SmallBot/SmallBot.js
+++ src/components/SmallBot/SmallBot.js
@@ -3,9 +3,18 @@ import './SmallBot.css';
 
 class SmallBot extends Component {
   render () {
+    const bots = [...this.props.bots];
+    const printBots = bots.map((bot) => {
+      return (
+        <div className='col-xs-1'>
+          <img src={bot.img} alt='robots'/>
+          <p>{bot.name}</p>
+        </div>
+      );
+    });
     return (
-      <div className="SmallBot">
-        <h1 className="SmallBot-title">SmallBot</h1>
+      <div>
+        {printBots}
       </div>
     );
   }
