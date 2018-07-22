5b3cc00ae93cd80de07bbf8e22b99d9f2bbfe801
Adam Wieckert <wiec1369@gmail.com>
2018-07-22 17:28:23 -0500

    Bot data is being passed to BattleBot component, health bar updated relative to % of health remaining. Need to add specials bar

--- src/components/BattleBot/BattleBot.js
+++ src/components/BattleBot/BattleBot.js
@@ -3,9 +3,20 @@ import './BattleBot.css';
 
 class BattleBot extends Component {
   render () {
+    const staticBot = {...this.props.staticBot};
+    const bot = {...this.props.bot};
+    const healthPercent = (bot.health / staticBot.health) * 100;
+    const healthStyle = {
+      width: `${healthPercent}%`,
+    };
     return (
-      <div className="BattleBot">
-        <h1 className="BattleBot-title">BattleBot</h1>
+      <div className="BattleBot col-xs-6">
+        <div className="progress">
+          <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={healthStyle}>
+            <span className="sr-only">60% Complete</span>
+          </div>
+        </div>
+        <img src={bot.img} alt="robot"/>
       </div>
     );
   }
--- src/components/FightArena/FightArena.js
+++ src/components/FightArena/FightArena.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import BattleBot from '../BattleBot/BattleBot.js';
 import './FightArena.css';
 
 class FightArena extends Component {
+  state = {
+    userRobot: {},
+    enemyRobot: {},
+    userStaticRobot: {},
+    enemyStaticRobot: {},
+  };
+
+  componentDidMount () {
+    const {userRobot} = {...this.props};
+    const {enemyRobot} = {...this.props};
+    this.setState({userRobot: userRobot});
+    this.setState({userStaticRobot: userRobot});
+    this.setState({enemyRobot: enemyRobot});
+    this.setState({enemyStaticRobot: enemyRobot});
+  };
+
   render () {
     return (
       <div className="FightArena">
         <h1 className="FightArena-title">FightArena</h1>
+        <div className='row'>
+          <BattleBot bot={this.state.userRobot} staticBot={this.state.userStaticRobot} />
+          <BattleBot bot={this.state.enemyRobot} staticBot={this.state.enemyStaticRobot} />
+        </div>
       </div>
     );
   }
