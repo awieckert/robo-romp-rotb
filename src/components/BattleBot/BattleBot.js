b7cde471810a52a72555e1c0150fc0375db80f93
Adam Wieckert <wiec1369@gmail.com>
2018-07-22 19:06:17 -0500

    Currently creating correct number of special bars per robot but the bars are not rendering properly.

--- src/components/BattleBot/BattleBot.js
+++ src/components/BattleBot/BattleBot.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
+import SpecialBar from '../SpecialBar/SpecialBar.js';
 import './BattleBot.css';
 
 class BattleBot extends Component {
-  render () {
-    const staticBot = {...this.props.staticBot};
-    const bot = {...this.props.bot};
+
+  determineHealth = (bot, staticBot) => {
     const healthPercent = (bot.health / staticBot.health) * 100;
     const healthStyle = {
       width: `${healthPercent}%`,
     };
+    return healthStyle;
+  };
+
+  setSpecialBar = (bot) => {
+    const barFull = {
+      width: '100%',
+    };
+    const barEmpty = {
+      width: '0%',
+    };
+    let specialBars = '';
+    for (let i = 1; i <= bot.specialCount; i++) {
+      if (bot.attackCount >= i) {
+        specialBars += <SpecialBar bar={barFull} />;
+      } else {
+        specialBars += <SpecialBar bar={barEmpty} />;
+      }
+    }
+    return specialBars;
+  };
+
+  render () {
+    const staticBot = {...this.props.staticBot};
+    const bot = {...this.props.bot};
+    const healthRemaining = this.determineHealth(bot, staticBot);
+    const specialBar = this.setSpecialBar(bot);
     return (
       <div className="BattleBot col-xs-6">
         <div className="progress">
-          <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={healthStyle}>
+          <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={healthRemaining}>
             <span className="sr-only">60% Complete</span>
           </div>
         </div>
         <img src={bot.img} alt="robot"/>
+        {specialBar}
       </div>
     );
   }
