8d5628b02bd9b04b7ebbe921f3c4e1773ba96789
Adam Wieckert <wiec1369@gmail.com>
2018-07-27 17:34:24 -0500

    Lost

--- src/components/FavoriteBots/FavoriteBots.js
+++ src/components/FavoriteBots/FavoriteBots.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 class FavoriteBots extends React.Component {
+  state = {
+    favoriteBots: [],
+  };
 
-  render () {
-    const sortedBots = [...this.props.favoriteBots];
+  filterFunction = () => {
+    const sortedBots = [...this.state.favoriteBots];
     const arrayToMap = [];
     for (let i = 0; i < 3; i++) {
       arrayToMap.push(sortedBots[i]);
     }
-    const botsToPrint = arrayToMap.map((bot) => {
+    return arrayToMap;
+  };
+
+  componentDidMount () {
+    const favoriteBots = {...this.props.favoriteBots};
+    this.setState({favoriteBots: favoriteBots});
+  };
+
+  render () {
+    const pleaseMap = this.filterFunction();
+    const botsToPrint = pleaseMap.map((bot) => {
       return (
         <div className='col-xs-2'>
           <img src={bot.img} alt="robot"/>
